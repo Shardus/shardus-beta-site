5e7e92263fb61687ab6d4857c0e0eef00c927b61
Thant Sin Toe <mr.thantsintoe@gmail.com>
2018-12-18 15:02:19 +0000

    show the user the number of output currency they will receive before doing the swap

--- assets/js/uniswap.js
+++ assets/js/uniswap.js
@@ -1,4 +1,4 @@
-/* global BigNumber, Web3, factoryABI, exchangeABI */
+/* global BigNumber, Web3, factoryABI, exchangeABI, tokenABI */
 
 let web3 = new Web3(Web3.givenProvider || 'ws://some.local-or-remote.node:8546');
 let swapForm = document.getElementById('uniswap-form')
@@ -9,10 +9,15 @@ const ULTtokenAddress = '0x09617f6fd6cf8a71278ec86e23bbab29c04353a7' // ULT Toke
 const ULTexchangeAddress = '0x28d9353611C5A0d5a026A648c05E5d6523e41CBf'
 
 let exchangeContract = new web3.eth.Contract(exchangeABI, ULTexchangeAddress)
+let tokenContract = new web3.eth.Contract(tokenABI, ULTtokenAddress)
 const ALLOWED_SLIPPAGE = 0.025;
 
-async function swap (type, inputValue) {
+let inputCurrency
+let outputCurrency
+let inputValue
+let outputValue
 
+async function swap (type, inputValue) {
     const blockNumber = await web3.eth.getBlockNumber()
     const block = await web3.eth.getBlock(blockNumber)
     const deadline =  block.timestamp + 300;
@@ -38,6 +43,7 @@ async function swap (type, inputValue) {
             })
     }
 }
+
 swapForm.addEventListener('submit', (event) => {
     event.preventDefault()
     let from = document.getElementById('convert-from').value
@@ -49,3 +55,41 @@ swapForm.addEventListener('submit', (event) => {
     else return
     swap(swapType, inputValue)
 })
+
+document.getElementById('swap-amount').addEventListener('change', updateUI)
+
+async function getOutputValue(inputCurrency, outputCurrency, inputValue) {
+    const inputAmount = inputValue
+    let type
+    let inputReserve
+    let outputReserve
+    if (inputCurrency === 'ETH') type = 'ETH_TO_TOKEN'
+    else if (outputCurrency === 'ETH') type = 'TOKEN_TO_ETH'
+    else if (inputCurrency !== 'ETH' && outputCurrency !== 'ETH') type = 'TOKEN_TO_TOKEN'
+    if (type === 'ETH_TO_TOKEN') {
+        inputReserve = await web3.eth.getBalance(ULTexchangeAddress)
+        outputReserve = await tokenContract.methods.balanceOf(ULTexchangeAddress).call()
+    } else if (type === 'TOKEN_TO_ETH') {
+        inputReserve = await tokenContract.methods.balanceOf(ULTexchangeAddress).call()
+        outputReserve = await web3.eth.getBalance(ULTexchangeAddress)
+    }
+    // Output amount
+    let numerator = inputAmount * outputReserve * 997
+    let denominator = inputReserve * 1000 + inputAmount * 997
+    const outputAmount = numerator / denominator
+    return outputAmount
+}
+
+async function updateUI() {
+    let { inputCurrency, outputCurrency, inputValue } = getDataFromUI()
+    const output = await getOutputValue(inputCurrency, outputCurrency, inputValue)
+    document.getElementById('outputValue').textContent = output.toFixed(6)
+    document.getElementById('outputUnit').textContent = outputCurrency
+}
+
+function getDataFromUI() {
+    inputCurrency = document.getElementById('convert-from').value
+    outputCurrency = document.getElementById('convert-to').value
+    inputValue = document.getElementById('swap-amount').value
+    return { inputCurrency, outputCurrency, inputValue }
+}
