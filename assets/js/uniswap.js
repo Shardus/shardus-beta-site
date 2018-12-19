5e65ffa66bf5b065a3bfbff8faec985abe4c72b4
Thant Sin Toe <mr.thantsintoe@gmail.com>
2018-12-19 14:57:19 +0000

    added more tokens and allow token to token swap. Update UI according to spec

--- assets/js/uniswap.js
+++ assets/js/uniswap.js
@@ -1,29 +1,54 @@
-/* global BigNumber, Web3, factoryABI, exchangeABI, tokenABI */
+/* global BigNumber, Web3, factoryABI, exchangeABI, tokenABI, tokenDB, $ */
 
 let web3 = new Web3(Web3.givenProvider || 'ws://some.local-or-remote.node:8546');
-let swapForm = document.getElementById('uniswap-form')
+let exchangeAddresses = tokenDB.exchangeAddresses
+let tokenSymbols = Object.keys(exchangeAddresses)
+let tokenAddressess = {}
+let exchangeContracts = {}
+let tokenContracts = {}
 
-// const factoryAddress = "0xf5D915570BC477f9B8D6C0E980aA81757A3AaC36" // Rinkeby Testnet
-const factoryAddress = '0xc0a47dFe034B400B47bDaD5FecDa2621de6c4d95' // Ethereum Mainet 
-const ULTtokenAddress = '0x09617f6fd6cf8a71278ec86e23bbab29c04353a7' // ULT Token
-const ULTexchangeAddress = '0x28d9353611C5A0d5a026A648c05E5d6523e41CBf'
+for (let i = 0; i < tokenSymbols.length; i += 1) {
+    exchangeContracts[tokenSymbols[i]] = new web3.eth.Contract(exchangeABI, exchangeAddresses[tokenSymbols[i]])
+}
 
-let exchangeContract = new web3.eth.Contract(exchangeABI, ULTexchangeAddress)
-let tokenContract = new web3.eth.Contract(tokenABI, ULTtokenAddress)
-const ALLOWED_SLIPPAGE = 0.025;
+tokenSymbols.forEach(async token => {
+    const contract = exchangeContracts[token]
+    tokenAddressess[token] = await contract.methods.tokenAddress().call()
+    tokenContracts[token] = new web3.eth.Contract(tokenABI, tokenAddressess[token])
+})
 
-let inputCurrency
-let outputCurrency
-let inputValue
-let outputValue
+function init() {
+    drawUI()
+    $('#convert-btn').on('click', function(e) {
+        let data = {}
+        $('#uniswap-form')
+            .serializeArray()
+            .forEach(input => {
+                data[input.name]= input.value
+            })
+        swap(data)
+    })
+}
 
-async function swap (type, inputValue) {
+function getSwapType(inputCurrency, outputCurrency) {
+    if (inputCurrency !== 'ETH' && outputCurrency === 'ETH') return 'TOKEN_TO_ETH'
+    else if (inputCurrency === 'ETH' && outputCurrency !== 'ETH') return 'ETH_TO_TOKEN'
+    else if (inputCurrency !== 'ETH' && outputCurrency !== 'ETH') return 'TOKEN_TO_TOKEN'
+}
+
+async function swap (data) {
+    let { inputValue, inputCurrency, outputValue, outputCurrency } = data
+    let type = getSwapType(inputCurrency, outputCurrency)
+    const ALLOWED_SLIPPAGE = 0.025;
+    
     const blockNumber = await web3.eth.getBlockNumber()
     const block = await web3.eth.getBlock(blockNumber)
     const deadline =  block.timestamp + 300;
     const accounts = await web3.eth.getAccounts()
+    let exchangeContract
     
-    if (type === 'ETH_TO_ULT') {
+    if (type === 'ETH_TO_TOKEN') {
+        exchangeContract = exchangeContracts[outputCurrency]
         const max_tokens = new BigNumber(100000).toFixed(0)
         const amount = new BigNumber(inputValue).multipliedBy(10 ** 18).toFixed(0)
         
@@ -32,7 +57,8 @@ async function swap (type, inputValue) {
                 if (err) console.log(err)
                 else alert('Swap from ETH to ULT is submitted...')
             })
-    } else if (type === 'ULT_TO_ETH') {
+    } else if (type === 'TOKEN_TO_ETH') {
+        exchangeContract = exchangeContracts[inputCurrency]
         const tokenSold = new BigNumber(inputValue).multipliedBy(10 ** 18).toFixed(0)
         const minEth = new BigNumber(2).toFixed(0)
         
@@ -41,55 +67,140 @@ async function swap (type, inputValue) {
               if (err) console.log(err) 
               else alert('Swap from ULT to ETH is submitted...')
             })
+    } else if (type === 'TOKEN_TO_TOKEN') {
+        exchangeContract = exchangeContracts[inputCurrency]
+        const tokenSold = new BigNumber(inputValue).multipliedBy(10 ** 18).toFixed(0)
+        const minToken = new BigNumber(outputValue).multipliedBy(10 ** 18).multipliedBy(1 - ALLOWED_SLIPPAGE).toFixed(0)
+        const minEth = new BigNumber(1).toFixed(0)
+        const outputTokenAddress = tokenAddressess[outputCurrency]
+        
+        exchangeContract.methods.tokenToTokenSwapInput(
+            tokenSold,
+            minToken,
+            minEth,
+            deadline,
+            outputTokenAddress
+        ).send({ from: accounts[0] }, (err, data) => {
+              if (err) console.log(err) 
+              else alert('Swap from Token to Token is submitted...')
+            })
     }
 }
-
-swapForm.addEventListener('submit', (event) => {
-    event.preventDefault()
-    let from = document.getElementById('convert-from').value
-    let to = document.getElementById('convert-to').value
-    let inputValue = document.getElementById('swap-amount').value
-    let swapType
-    if (from === 'ULT' && to === 'ETH') swapType = 'ULT_TO_ETH'
-    else if (from === 'ETH' && to === 'ULT') swapType = 'ETH_TO_ULT'
-    else return
-    swap(swapType, inputValue)
-})
-
-document.getElementById('swap-amount').addEventListener('change', updateUI)
 
 async function getOutputValue(inputCurrency, outputCurrency, inputValue) {
     const inputAmount = inputValue
-    let type
-    let inputReserve
-    let outputReserve
-    if (inputCurrency === 'ETH') type = 'ETH_TO_TOKEN'
-    else if (outputCurrency === 'ETH') type = 'TOKEN_TO_ETH'
-    else if (inputCurrency !== 'ETH' && outputCurrency !== 'ETH') type = 'TOKEN_TO_TOKEN'
+    const type = getSwapType(inputCurrency, outputCurrency)
+
     if (type === 'ETH_TO_TOKEN') {
-        inputReserve = await web3.eth.getBalance(ULTexchangeAddress)
-        outputReserve = await tokenContract.methods.balanceOf(ULTexchangeAddress).call()
+        let tokenExchangeAddress = exchangeAddresses[outputCurrency]
+        let tokenContract = tokenContracts[outputCurrency]
+        let inputReserve = await web3.eth.getBalance(tokenExchangeAddress)
+        let outputReserve = await tokenContract.methods.balanceOf(tokenExchangeAddress).call()
+
+        let numerator = inputAmount * outputReserve * 997
+        let denominator = inputReserve * 1000 + inputAmount * 997
+        const outputAmount = numerator / denominator
+        return outputAmount
     } else if (type === 'TOKEN_TO_ETH') {
-        inputReserve = await tokenContract.methods.balanceOf(ULTexchangeAddress).call()
-        outputReserve = await web3.eth.getBalance(ULTexchangeAddress)
+        let tokenExchangeAddress = exchangeAddresses[inputCurrency]
+        let tokenContract = tokenContracts[inputCurrency]
+        let inputReserve = await tokenContract.methods.balanceOf(tokenExchangeAddress).call()
+        let outputReserve = await web3.eth.getBalance(tokenExchangeAddress)
+        
+        let numerator = inputAmount * outputReserve * 997
+        let denominator = inputReserve * 1000 + inputAmount * 997
+        const outputAmount = numerator / denominator
+        return outputAmount
+    } else if (type === 'TOKEN_TO_TOKEN') {
+        let inputAmountA = inputValue
+        let tokenContractA = tokenContracts[inputCurrency]
+        let exchangeAddressA = exchangeAddresses[inputCurrency]
+        let tokenContractB = tokenContracts[outputCurrency]
+        let exchangeAddressB = exchangeAddresses[outputCurrency]
+        
+        // TokenA (ERC20) to ETH conversion
+        let inputReserveA = await tokenContractA.methods.balanceOf(exchangeAddressA).call()
+        let outputReserveA = await web3.eth.getBalance(exchangeAddressA)
+
+        let numeratorA = inputAmountA * outputReserveA * 997
+        let denominatorA = inputReserveA * 1000 + inputAmountA * 997
+        let outputAmountA = numeratorA / denominatorA
+        
+        // ETH to TokenB conversion 
+        let inputAmountB = outputAmountA    
+        let inputReserveB = await web3.eth.getBalance(exchangeAddressB)
+        let outputReserveB = await tokenContractB.methods.balanceOf(exchangeAddressB).call()
+
+        let numeratorB = inputAmountB * outputReserveB * 997
+        let denominatorB = inputReserveB * 1000 + inputAmountB * 997
+        let outputAmountB = numeratorB / denominatorB
+        
+        return outputAmountB
     }
-    // Output amount
-    let numerator = inputAmount * outputReserve * 997
-    let denominator = inputReserve * 1000 + inputAmount * 997
-    const outputAmount = numerator / denominator
-    return outputAmount
 }
 
-async function updateUI() {
-    let { inputCurrency, outputCurrency, inputValue } = getDataFromUI()
-    const output = await getOutputValue(inputCurrency, outputCurrency, inputValue)
-    document.getElementById('outputValue').textContent = output.toFixed(6)
-    document.getElementById('outputUnit').textContent = outputCurrency
+function drawUI() {
+    let selectHTML = `<a class="dropdown-item" token-name="ETH" href="#">ETH</a>`
+    for (let i = 0; i < tokenSymbols.length; i += 1) {
+        selectHTML += `<a class="dropdown-item" token-name="${tokenSymbols[i]}" href="#">${tokenSymbols[i]}</a>`
+    }
+    $('#uniswap-form .dropdown-menu').html(selectHTML)
+    setTimeout(() => {
+        calculateULTPrice().then(price => $('#ULT-price').html(`<strong>${price.toFixed(6)}</strong> $`))
+    }, 3000)
 }
 
-function getDataFromUI() {
-    inputCurrency = document.getElementById('convert-from').value
-    outputCurrency = document.getElementById('convert-to').value
-    inputValue = document.getElementById('swap-amount').value
-    return { inputCurrency, outputCurrency, inputValue }
+async function calculateULTPrice() {
+    let price = await getOutputValue('ULT', 'DAI', 1)
+    return price
 }
+
+// start listening events on widget
+init ()
+
+$('#swapModal').on('show.bs.modal', function (event) {
+  var button = $(event.relatedTarget) // Button that triggered the modal
+  var action = button.data('action') // Extract info from data-* attributes
+  var modal = $(this)
+  modal.find('.modal-title').text(`${action} ULT`)
+  modal.find('.modal-body input').val('')
+  if (action === 'buy') {
+      $('.pay-group button').attr('disabled', false)
+      $('.receive-group button').attr('disabled', true)
+      $('#inputCurrency').val('ETH')
+      $('#outputCurrency').val('ULT')
+      $('#input-select-btn').text('ETH')
+      $('#output-select-btn').text('ULT')
+      $('#convert-btn').text('BUY')
+  } else if (action === 'sell') {
+      $('.pay-group button').attr('disabled', true)
+      $('.receive-group button').attr('disabled', false)
+      $('#inputCurrency').val('ULT')
+      $('#outputCurrency').val('ETH')
+      $('#input-select-btn').text('ULT')
+      $('#output-select-btn').text('ETH')
+      $('#convert-btn').text('SELL')
+  }
+})
+
+$('.pay-group .dropdown-menu .dropdown-item').on('click', function(e) {
+    e.preventDefault()
+    const selectedToken = this.getAttribute('token-name')
+    $('#inputCurrency').val(selectedToken)
+    $('#input-select-btn').text(selectedToken)
+})
+
+$('.receive-group .dropdown-menu .dropdown-item').on('click', function(e) {
+    e.preventDefault()
+    const selectedToken = this.getAttribute('token-name')
+    $('#outputCurrency').val(selectedToken)
+    $('#output-select-btn').text(selectedToken)
+})
+
+$('#inputValue').on('change', async function(e) {
+    const inputValue = $(this).val()
+    const inputCurrency = $('#inputCurrency').val()
+    const outputCurrency = $('#outputCurrency').val()
+    const outputValue = await getOutputValue(inputCurrency, outputCurrency, inputValue)
+    $('#outputValue').val(outputValue)
+})
