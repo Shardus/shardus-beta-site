b5d08386d1683657dd5ccb777d940d9c2c8bac33
Trebilcode <d11a2n3i4e5l6@hotmail.com>
2020-09-08 11:52:23 -0500

    Initial commit with cra

---
+++ src/components/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+import styles from './navbar.module.css'
+
+const Navbar = () => {
+  return(
+    <nav className={styles.navbar}>
+      <ul>
+        <li>HOME</li>
+        <li>EVENT</li>
+        <li>TECHNOLOGY</li>
+        <li>FEATURES</li>
+        <li>TOKEN</li>
+        <li>BOUNTIES</li>
+        <li>ROADMAP</li>
+        <li>TEAM</li>
+        <li>FAQ</li>
+        <li>JOIN</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
+
